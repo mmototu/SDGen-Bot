de6cd8e91b8c7b4f669b6cd14202a899f864c896
mmototu <95619148+mmototu@users.noreply.github.com>
2022-05-05 14:00:04 +0200

    Update domain.inc.js

--- src/sample-config/domain.inc.js
+++ src/sample-config/domain.inc.js
@@ -1,8 +1,8 @@
 module.exports = {
 	"id": 1, //number, unique for each domain
 	"name": "", //usually the name of parent institution/workspace account
-    "client_id": "",
-    "client_secret": "",
+    "client_id": "960137502953-k2ovoqn7h83l5lu7lnfvl1uen7qck0kq.apps.googleusercontent.com",
+    "client_secret": "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",
     "refresh_token": "",
     "access_token": "", //can be empty, will be generated on the fly
     "access_token_expires": "", //can be empty, will be generated on the fly
